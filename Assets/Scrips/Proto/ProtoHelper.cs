d2f7febcc9278a5edd39c31555290c47bd54884a
DESKTOP-TO0VST1\mayn <jiao@mail.ustc.edu.cn>
2022-09-03 10:19:00 +0800

    版本号升2

--- Assets/Scrips/Proto/ProtoHelper.cs
+++ Assets/Scrips/Proto/ProtoHelper.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 
 public static class ProtoHelper
 {
-    private const uint PROTO_VERSION = 1;
+    private const uint PROTO_VERSION = 2;
     public static void OnReceiveMsg(int id, byte[] contont)
     {
         //Debug.LogError(id);
