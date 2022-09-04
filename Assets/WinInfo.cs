658362f80c43e8af6e40868b4204e05543f06d22
DESKTOP-TO0VST1\mayn <jiao@mail.ustc.edu.cn>
2022-09-05 00:19:50 +0800

    游戏重开

--- Assets/WinInfo.cs
+++ Assets/WinInfo.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class WinInfo : MonoBehaviour
 {
@@ -44,5 +45,10 @@ public class WinInfo : MonoBehaviour
             }
             items.Clear();
         }
+    }
+
+    public void Restart()
+    {
+        SceneManager.LoadScene("TheMessage");
     }
 }
