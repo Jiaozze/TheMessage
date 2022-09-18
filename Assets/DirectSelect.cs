75e30702b8f27d00282fadfb0afe195456415ee2
DESKTOP-TO0VST1\mayn <jiao@mail.ustc.edu.cn>
2022-09-19 05:21:59 +0800

    老鳖情报方向

--- Assets/DirectSelect.cs
+++ Assets/DirectSelect.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -11,36 +12,6 @@ public class DirectSelect : MonoBehaviour
 
     private void OnEnable()
     {
-        if (!GameManager.Singleton.players.ContainsKey(GameManager.SelfPlayerId))
-        {
-            return;
-        }
-        RoleBase role = GameManager.Singleton.players[GameManager.SelfPlayerId].role;
-        if (role == null)
-        {
-            return;
-        }
-
-        if (role is Role_LaoBie)
-        {
-            Role_LaoBie role_LaoBie = role as Role_LaoBie;
-            if(GameManager.Singleton.cardsHand.ContainsKey(GameManager.Singleton.SelectCardId))
-            {
-                role_LaoBie.direction = GameManager.Singleton.cardsHand[GameManager.Singleton.SelectCardId].direction;
-            }
-            switch (role_LaoBie.direction)
-            {
-                case DirectionEnum.Left:
-                    tgLeft.isOn = true;
-                    break;
-                case DirectionEnum.Up:
-                    tgUp.isOn = true;
-                    break;
-                case DirectionEnum.Right:
-                    tgRight.isOn = true;
-                    break;
-            }
-        }
     }
 
     public void OnToggleLeft(bool isOn)
@@ -83,6 +54,73 @@ public class DirectSelect : MonoBehaviour
         {
             Role_LaoBie role_LaoBie = role as Role_LaoBie;
             role_LaoBie.direction = direction;
+            SetBanClick(direction);
         }
+    }
+
+    private void SetBanClick(DirectionEnum dir)
+    {
+        bool banClick = false;
+        switch (dir)
+        {
+            case DirectionEnum.Left:
+                foreach (var kv in GameManager.Singleton.gameUI.Players)
+                {
+                    banClick = kv.Key != GameManager.Singleton.GetPlayerAliveLeft(GameManager.SelfPlayerId);
+                    kv.Value.SetBanClick(banClick);
+                }
+                break;
+            case DirectionEnum.Right:
+                foreach (var kv in GameManager.Singleton.gameUI.Players)
+                {
+                    banClick = kv.Key != GameManager.Singleton.GetPlayerAliveRight(GameManager.SelfPlayerId);
+                    kv.Value.SetBanClick(banClick);
+                }
+                break;
+            case DirectionEnum.Up:
+                foreach (var kv in GameManager.Singleton.gameUI.Players)
+                {
+                    kv.Value.SetBanClick(false);
+                }
+                break;
+        }
+
+    }
+
+    public void Check()
+    {
+        gameObject.SetActive(true);
+        if (!GameManager.Singleton.players.ContainsKey(GameManager.SelfPlayerId))
+        {
+            return;
+        }
+        RoleBase role = GameManager.Singleton.players[GameManager.SelfPlayerId].role;
+        if (role == null)
+        {
+            return;
+        }
+
+        if (role is Role_LaoBie)
+        {
+            Role_LaoBie role_LaoBie = role as Role_LaoBie;
+            if (GameManager.Singleton.cardsHand.ContainsKey(GameManager.Singleton.SelectCardId))
+            {
+                role_LaoBie.direction = GameManager.Singleton.cardsHand[GameManager.Singleton.SelectCardId].direction;
+            }
+            switch (role_LaoBie.direction)
+            {
+                case DirectionEnum.Left:
+                    tgLeft.isOn = true;
+                    break;
+                case DirectionEnum.Up:
+                    tgUp.isOn = true;
+                    break;
+                case DirectionEnum.Right:
+                    tgRight.isOn = true;
+                    break;
+            }
+            SetBanClick(role_LaoBie.direction);
+        }
+
     }
 }
