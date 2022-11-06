6c1f31dc0b976d7da12a11c88d0ededd25a3d2a3
DESKTOP-TO0VST1\mayn <jiao@mail.ustc.edu.cn>
2022-11-07 01:33:02 +0800

    协议版本号+1

--- Assets/Scrips/NetWork/ProtoHelper.cs
+++ Assets/Scrips/NetWork/ProtoHelper.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 
 public static class ProtoHelper
 {
-    private const uint PROTO_VERSION = 17;
+    private const uint PROTO_VERSION = 18;
     public static void OnReceiveMsg(int id, byte[] contont)
     {
         //GetIdFromProtoName("wait_for_select_role_toc");
