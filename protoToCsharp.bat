f0d1b4e241f6de02a8c67cd1bd1419bbd6f13ed9
奇葩の灵梦 <415551921@qq.com>
2023-02-09 11:58:09 +0800

    同步更新脚本

--- protoToCsharp.bat
+++ protoToCsharp.bat
@@ -2,7 +2,7 @@
 
 set "PROTOC_EXE=%cd%\ProtoTool\protoc.exe"
 set "WORK_DIR=%cd%\Assets\Network"
-set "CS_OUT_PATH=%cd%\Assets\Scrips\Proto"
+set "CS_OUT_PATH=%cd%\Assets\Scripts\Proto"
 ::if not exist %CS_OUT_PATH% md %CS_OUT_PATH%
 
 for /r %%i in ("Assets\Network\*.proto") do (
